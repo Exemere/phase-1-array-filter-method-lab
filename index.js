1457a315d8cc96e56fb8ce5b47b99a22bd863bcf
Livosero Esemere Nicolas <nesemere55@gmail.com>
2023-07-20 10:24:11 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+const drivers = [
+    { name: "nick" },
+    { name: "jan" },
+    { name: "mary"},
+];
+
+function findMatching(drivers, query) {
+    const lowerCaseQuery = query.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowerCaseQuery);
+}
+function fuzzyMatch(drivers, query) {
+    const lowerCaseQuery = query.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowerCaseQuery));
+}
+
+function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+}
